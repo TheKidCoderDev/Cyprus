bd252bfea7ec2f599f3741fbfa5557ef93285498
thekidcoderdev <immilse@gmail.com>
2019-09-23 18:22:05 -0400

    Add config stuff

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 apt-get install dnsmasq -y
 
 echo "What Top-Level Domain should we use: "
-read tld
+read TLD
 
-echo "127.0.0.1	admin.$tld" >> /etc/hosts
+IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
+
+echo "$IP	admin.$TLD" >> /etc/hosts
+
+echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
+echo "listen-address=$IP" >> /etc/dnsmasq.conf
