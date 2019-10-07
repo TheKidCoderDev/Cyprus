6e855b8c6cda68261b1e4ead09d56c445f424857
thekidcoderdev <immilse@gmail.com>
2019-10-07 16:11:36 -0400

    update

--- api.py
+++ api.py
@@ -2,6 +2,18 @@
 
 from bottle import run, get, post, request, route, response, hook, static_file
 import os
+import re
+import subprocess
+
+@hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = 'http://0.0.0.0:8080'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @get("/getDomains")
 def getDomains():
@@ -10,7 +22,7 @@ def getDomains():
     with open("/etc/hosts", "r") as f:
         lines = f.readlines()
     for line in lines:
-        domains.append(line.split("	").reverse())
+        domains.append(reversed(line.strip("\n").split("	")))
 
     return dict(domains)
 
@@ -23,7 +35,7 @@ def newDomain():
         if domain in f.read():
             return {'Error':"Domain taken"}
     with open('/etc/hosts', 'a') as f:
-        f.write("{}	{}".format(ip, domain))
+        f.write("{}	{}\n".format(ip, domain))
     return {}
 
 @post("/delDomain")
@@ -38,10 +50,9 @@ def delDomain():
                 f.write(line)
     return {}
 
-@post("/restart")
+@get("/restart")
 def restart():
     os.system("sudo service dnsmasq stop")
     os.system("sudo service dnsmasq start")
 
-
-run(reloader=True, debug=True, port=30, host='10.7.19.175')
+run(reloader=True, debug=True, port=30, host='0.0.0.0')
--- install.sh
+++ install.sh
@@ -6,12 +6,9 @@ apt-get install nodejs npm node-semver -y
 
 npm install -g http-server
 
-echo "What Top-Level Domain should we use: "
-read TLD
-
 IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 
-echo "$IP	admin.$TLD" > /etc/hosts
+echo "$IP	admin.cyp" > /etc/hosts
 
 echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
 echo "listen-address=$IP" >> /etc/dnsmasq.conf
