d2bbf3d1c118e2bfdecaf59bc572ba59da5bbf48
thekidcoderdev <immilse@gmail.com>
2019-10-07 18:17:50 -0400

    remove unused dependency

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 apt-get install dnsmasq -y
 apt-get install tmux -y
 
-npm install -g http-server
-
 IP="$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')"
 
 echo "$IP	admin.cyp" > /etc/hosts
